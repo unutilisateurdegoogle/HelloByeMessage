f6e60a5ae704ba6b29296e8993b47761c9f67134
unutilisateurdegoogle <74296926+unutilisateurdegoogle@users.noreply.github.com>
2021-06-14 07:42:08 +0200

    Main.cpp a c++ file.

---
+++ Main.cpp
@@ -0,0 +1,38 @@
+//Warning defillant text is here 
+//No color:(
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	system("ECHO H");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO He");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Hel");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Hell");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Hello");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Hello!");
+	Sleep(2000);
+	system("CLS");
+	Sleep(2000);
+	system("ECHO B");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO By");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Bye");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Bye.");
+	Sleep(2000);
+}
