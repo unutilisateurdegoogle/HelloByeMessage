ddec571ac0b2528155d8254456047ba292eff442
unutilisateurdegoogle <74296926+unutilisateurdegoogle@users.noreply.github.com>
2021-06-14 08:06:10 +0200

    Update Main (final).cpp

--- Main (final).cpp
+++ Main (final).cpp
@@ -1,40 +1,40 @@
-//Warning defillant text is here
-//Includes color!
+//Warning defillant text is here 
 #include <iostream>
 #include <windows.h>
 
 int main()
 {
+	system("title A message from (unknown) person.");
 	system("color 0a");
 	system("ECHO H");
-	Sleep(2000);
+	Sleep(1000);
 	system("CLS");
 	system("ECHO He");
-	Sleep(2000);
+	Sleep(1000);
 	system("CLS");
 	system("ECHO Hel");
-	Sleep(2000);
+	Sleep(1000);
 	system("CLS");
 	system("ECHO Hell");
-	Sleep(2000);
+	Sleep(1000);
 	system("CLS");
 	system("ECHO Hello");
-	Sleep(2000);
+	Sleep(1000);
 	system("CLS");
 	system("ECHO Hello!");
-	Sleep(2000);
+	Sleep(1000);
 	system("color 0c");
 	system("CLS");
-	Sleep(2000);
+	Sleep(1000);
 	system("ECHO B");
-	Sleep(2000);
+	Sleep(1000);
 	system("CLS");
 	system("ECHO By");
-	Sleep(2000);
+	Sleep(1000);
 	system("CLS");
 	system("ECHO Bye");
-	Sleep(2000);
+	Sleep(1000);
 	system("CLS");
 	system("ECHO Bye.");
-	Sleep(2000);
+	Sleep(1000);
 }
