a96dee20cc29e17d2aad07ed2d404db222143158
unutilisateurdegoogle <74296926+unutilisateurdegoogle@users.noreply.github.com>
2021-06-14 07:52:14 +0200

    Final

---
+++ Main (final).cpp
@@ -0,0 +1,40 @@
+//Warning defillant text is here
+//Includes color!
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	system("color 0a");
+	system("ECHO H");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO He");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Hel");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Hell");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Hello");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Hello!");
+	Sleep(2000);
+	system("color 0c");
+	system("CLS");
+	Sleep(2000);
+	system("ECHO B");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO By");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Bye");
+	Sleep(2000);
+	system("CLS");
+	system("ECHO Bye.");
+	Sleep(2000);
+}
